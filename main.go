04f419edddfe130f948477558115a8400da5fa68
wangliang <wormside@126.com>
2020-11-18 21:36:58 +0800

    自动重载

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog<h1>")
+		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！<h1>")
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w, "此博客是用以记录变编程笔记，如你有反馈或建议，请联系"+"<a href=\"mailto:wlight@gmail.com\">wlight</a>")
 	} else {
