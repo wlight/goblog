ae2c678d327f6c60e47de39afd8c26f8fec17511
wangliang <wormside@126.com>
2020-11-22 23:12:16 +0800

    使用中间件

--- main.go
+++ main.go
@@ -2,24 +2,19 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func hometHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	
-		fmt.Fprint(w, "<h1>Hello, World!<h1>")
-
-	}
+	fmt.Fprint(w, "<h1>Hello, World!<h1>")
+}
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, "此博客是用以记录变编程笔记，如你有反馈或建议，请联系\"+\"<a href=\\\"mailto:wlight@gmail.com\\\">wlight</a>")
 }
 
-func notFoundHandler(w http.ResponseWriter, r *http.Request)  {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, "<h1>请求页面未找到:(</h1>"+"<p>如有问题，请联系我们。</p>")
 }
@@ -30,12 +25,21 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "文章 ID："+id)
 }
 
-func articlesIndexHandler(w http.ResponseWriter, r *http.Request)  {
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "访问文章列表")
 }
 
-func articlesStoreHandler(w http.ResponseWriter, r *http.Request)  {
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "创建新的文章")
+}
+
+func forceHTMLMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// 1：设置标头
+		w.Header().Set("Content-type", "text/html; charset=utf-8")
+		// 2:继续处理请求
+		next.ServeHTTP(w, r)
+	})
 }
 
 func main() {
@@ -50,7 +54,11 @@ func main() {
 
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
 
+	// 自定义 404 页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+
+	// 中间件：强制内容类型为 html
+	router.Use(forceHTMLMiddleware)
 
 	// 通过命名路由获取URL示例
 	homeURL, _ := router.Get("home").URL()
