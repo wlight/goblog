36258b79f54d21e2a6bb24581cb0b3cd7f6a6bb4
wangliang <wormside@126.com>
2020-12-27 13:05:07 +0800

    重构中间件

--- main.go
+++ main.go
@@ -1,60 +1,15 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/mux"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/database"
 	"net/http"
-	"strings"
 )
 
-var router *mux.Router
-var db *sql.DB
-
-func hometHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, World!<h1>")
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-}
-
-func notFoundHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>请求页面未找到:(</h1>"+"<p>如有问题，请联系我们。</p>")
-}
-
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1：设置标头
-		w.Header().Set("Content-type", "text/html; charset=utf-8")
-		// 2:继续处理请求
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// 1. 除首页以外，移除所有请求路径后面的斜杆
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-		// 2.将请求传递下去
-		next.ServeHTTP(w, r)
-	})
-}
-
 func main() {
-	database.Initialize()
-	db = database.DB
-
 	bootstrap.SetupDB()
-	router = bootstrap.SetupRoute()
+	router := bootstrap.SetupRoute()
 
-	// 中间件：强制内容类型为 html
-	router.Use(forceHTMLMiddleware)
-
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
