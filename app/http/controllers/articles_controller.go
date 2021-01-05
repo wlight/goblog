20e44819afad98f01dc6879bea94052ea8a04e49
wangliang <wormside@126.com>
2021-01-05 22:38:33 +0800

    注册页面

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -57,15 +57,15 @@ func (*ArticlesController) Index(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// ArticlesFormData 创建博文表单数据
-type ArticlesFormData struct {
-	Title, Body string
-	Article         article.Article
-	Errors      map[string]string
-}
+//// ArticlesFormData 创建博文表单数据
+//type ArticlesFormData struct {
+//	Title, Body string
+//	Article         article.Article
+//	Errors      map[string]string
+//}
 
 func (*ArticlesController) Create(w http.ResponseWriter, r *http.Request) {
-	view.Render(w, ArticlesFormData{}, "articles.create", "articles._form_field" )
+	view.Render(w, view.D{}, "articles.create", "articles._form_field" )
 }
 
 func validateArticleFormData(title string, body string) map[string]string {
@@ -108,10 +108,10 @@ func (*ArticlesController) Store(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprint(w, "创建文章失败，请联系管理员")
 		}
 	} else {
-		view.Render(w, ArticlesFormData{
-			Title: title,
-			Body: body,
-			Errors: errors,
+		view.Render(w, view.D{
+			"Title": title,
+			"Body": body,
+			"Errors": errors,
 		}, "articles.create", "artilces._form_field")
 	}
 }
@@ -138,11 +138,11 @@ func (*ArticlesController) Edit(w http.ResponseWriter, r *http.Request) {
 	} else {
 
 		// 4 读取成功，显示表单
-		view.Render(w, ArticlesFormData{
-			Title: _article.Title,
-			Body: _article.Body,
-			Article: _article,
-			Errors: nil,
+		view.Render(w, view.D{
+			"Title": _article.Title,
+			"Body": _article.Body,
+			"Article": _article,
+			"Errors": nil,
 		}, "articles.edit", "articles._form_field")
 	}
 }
@@ -196,11 +196,11 @@ func (*ArticlesController) Update(w http.ResponseWriter, r *http.Request) {
 			}
 		} else {
 			// 4.3 表单验证不通过，显示理由
-			view.Render(w, ArticlesFormData{
-				Title: _article.Title,
-				Body: _article.Body,
-				Article: _article,
-				Errors: errors,
+			view.Render(w, view.D{
+				"Title": _article.Title,
+				"Body": _article.Body,
+				"Article": _article,
+				"Errors": errors,
 			}, "articles.edit", "articles._form_field")
 		}
 	}
@@ -246,7 +246,5 @@ func (*ArticlesController) Delete(w http.ResponseWriter, r *http.Request) {
 				fmt.Fprint(w, "404 文章未找到")
 			}
 		}
-
 	}
-
 }
