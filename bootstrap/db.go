fc9a934e20b4506a71373e519003461c8a4d356c
wangliang <wormside@126.com>
2021-01-11 21:35:46 +0800

    创建用户

--- bootstrap/db.go
+++ bootstrap/db.go
@@ -1,7 +1,10 @@
 package bootstrap
 
 import (
+	"goblog/app/models/article"
+	"goblog/app/models/user"
 	"goblog/pkg/model"
+	"gorm.io/gorm"
 	"time"
 )
 
@@ -19,4 +22,11 @@ func SetupDB() {
 	sqlDB.SetConnMaxIdleTime(25)
 	// 设置每个连接的过期时间
 	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+	// 创建和维护数据表结构
+	migration(db)
+}
+
+func migration(db *gorm.DB) {
+	// 自动迁移
+	db.AutoMigrate(&user.User{}, &article.Article{})
 }
