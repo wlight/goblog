7591f3642d01bf0ce8d28a24392fbfd43d929c3b
wangliang <wormside@126.com>
2021-01-12 22:09:19 +0800

    登录表单和会话控制

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -33,14 +33,14 @@ func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 	if len(errs) > 0 {
 		// 4.1 有错误发生，大隐书局
 		view.RenderSimple(w, view.D{
-			"Errors":errs,
-			"User":_user,
+			"Errors": errs,
+			"User":   _user,
 		}, "auth.register")
 	} else {
 		//4.2 验证通过--入库，并跳转到首页
 		_user.Create()
 		if _user.ID > 0 {
-			http.Redirect(w,r, "/", http.StatusFound)
+			http.Redirect(w, r, "/", http.StatusFound)
 		} else {
 			w.WriteHeader(http.StatusInternalServerError)
 			fmt.Fprint(w, "注册失败，请联系管理员")
@@ -48,4 +48,14 @@ func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// 3. 表单不通过--重新显示表单
+}
+
+// 显示登录表单
+func (*AuthController) Login(w http.ResponseWriter, r *http.Request) {
+	view.RenderSimple(w, view.D{}, "auth.login")
+}
+
+// 处理登录表单提交
+func (*AuthController) DoLogin(w http.ResponseWriter, r *http.Request) {
+	//
 }
